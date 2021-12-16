2415e3ba40b6ffe6a69287be5608e53c4912ee15
kamruz-zaman8991 <70217992+kamruz-zaman8991@users.noreply.github.com>
2021-12-16 17:54:24 +0600

    status text add successfull

--- Server.js
+++ Server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 require('dotenv').config()
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 const { MongoClient } = require('mongodb');
-
+app.use(cors());
+app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.utqcf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -12,16 +14,17 @@ async function run() {
     try {
         await client.connect();
         const database = client.db("Atgworld");
-        const haiku = database.collection("Status");
+        const statusCollection = database.collection("Status");
 
         // post a Status
-        app.post('/drones', async (req, res) => {
+        app.post('/status', async (req, res) => {
             const data = req.body;
-            const package = await packagesCollection.insertOne(data);
-            res.send(package);
+            console.log(data);
+            const status = await statusCollection.insertOne(data);
+            res.send(status);
         });
     } finally {
-        // await client.close();
+        // await client.close();f
     }
 }
 run().catch(console.dir);
